bcc15a996d1a636df1fc68f538cdac7272bbd40d
Prasanta <pkd.dapacific@gmail.com>
2020-07-25 18:33:53 +0530

    gh-pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9994afb83c0f2832aa93";
+/******/ 	var hotCurrentHash = "0c13902c2345aa3c9cd7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
